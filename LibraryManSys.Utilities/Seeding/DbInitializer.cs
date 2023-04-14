0385d2beb0bdf1b2caf7c79f620534799457e1ac
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-04-14 02:19:40 +0200

    Add project files.

---
+++ LibraryManSys.Utilities/Seeding/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManSys.Utilities.Seeding
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+        https://www.youtube.com/watch?v=KYWIpJfvoeo&t=185s
+            18:25 süre
+        }
+    }
+}
